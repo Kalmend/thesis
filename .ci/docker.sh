7ed05b7aa86fa0834687d6efb6d5d8ad9f2b0ea3
Oliver Kalmend <kalmendoliver@gmail.com>
2016-02-03 20:39:45 +0200

    ci:simplified docker

--- .ci/docker.sh
+++ .ci/docker.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-PROJECT_NAME="my_ros_project"
+#PROJECT_NAME="my_ros_project"
 # init workspace
 source /opt/ros/jade/setup.bash
-# Run roscore
-roscore &
-cd /test/src
-catkin_init_workspace
-cd /test/
+## Run roscore
+#roscore &
+cd /test/workspace
 # Build with coverage support
-catkin_make --cmake-args -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage"
-sleep 60
-# Run tests
-catkin_make run_tests
-sleep 30
-# Move test reports to results
-mkdir /results/gtest_reports/
-mv /test/build/test_results/* /results/gtest_reports/
-# Generate coverage report
-cd build/${PROJECT_NAME}/CMakeFiles/${PROJECT_NAME}-test.dir/src/
-gcovr --xml -o coverage_report.xml -r /test/src/${PROJECT_NAME}/src/ .
-# Move coverage report
-mv coverage_report.xml /results/
-# Move .cpp files
-mv /test/src/${PROJECT_NAME}/src/* /results/
-# Replace source directory in report
-sed -i 's/\/test\/src\/'${PROJECT_NAME}'\/src\//results\//g' /results/coverage_report.xml
+./make_all_debug.sh
+#catkin_make --cmake-args -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage"
+#sleep 60
+## Run tests
+#catkin_make run_tests
+#sleep 30
+## Move test reports to results
+#mkdir /results/gtest_reports/
+#mv /test/build/test_results/* /results/gtest_reports/
+## Generate coverage report
+#cd build/${PROJECT_NAME}/CMakeFiles/${PROJECT_NAME}-test.dir/src/
+#gcovr --xml -o coverage_report.xml -r /test/src/${PROJECT_NAME}/src/ .
+## Move coverage report
+#mv coverage_report.xml /results/
+## Move .cpp files
+#mv /test/src/${PROJECT_NAME}/src/* /results/
+## Replace source directory in report
+#sed -i 's/\/test\/src\/'${PROJECT_NAME}'\/src\//results\//g' /results/coverage_report.xml
 chmod -R 777 /results/*
