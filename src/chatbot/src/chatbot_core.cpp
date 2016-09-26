843064e1f73045f7a9cebeb00ff69f733f343746
Oliver Kalmend <kalmendoliver@gmail.com>
2016-09-27 02:03:33 +0300

    moved operation before preempt, to allow cancellation

--- src/chatbot/src/chatbot_core.cpp
+++ src/chatbot/src/chatbot_core.cpp
@@ -21,31 +21,29 @@ void ChatCore::executeGotoCB(const chatbot::NamedMoveBaseGoalConstPtr &goal)
 {
 	bool success = true;
     auto x = goal->goal_move_base.target_pose.pose.position.x;
-    auto y = goal->goal_move_base.target_pose.pose.position.y;
-    auto name = goal->goal_name;
-    // publish info to the console for the user
-    ROS_INFO("ChatCore::goto: %s[%.2f,%.2f] received from prolog!", name.c_str(), x, y);
+	auto y = goal->goal_move_base.target_pose.pose.position.y;
+	auto name = goal->goal_name;
+	// publish info to the console for the user
+	ROS_INFO("ChatCore::goto: %s[%.2f,%.2f] received from prolog!", name.c_str(), x, y);
 
-    // start executing the action
+	// start executing the action
+	while (!gotoAc_.waitForServer(ros::Duration(5.0)))
+		ROS_INFO("ChatCore:goto: goto received, but no move_base server. Waiting for the move_base action server to come up");
 
-    // check that preempt has not been requested by the client
-    if (gotoAs_.isPreemptRequested() || !ros::ok())
-    {
-    	ROS_INFO("ChatCore: goto cancelled!");
-        // set the action state to preempted
-        gotoAs_.setPreempted();
-        success = false;
-    }
+	ROS_INFO("ChatCore::goto: sending goal to planner.");
+	gotoAc_.sendGoal(goal->goal_move_base);
+	gotoAc_.waitForResult();
+	// check that preempt has not been requested by the client
+	if (gotoAs_.isPreemptRequested() || !ros::ok())
+	{
+		ROS_INFO("ChatCore: goto cancelled!");
+		// set the action state to preempted
+		gotoAs_.setPreempted();
+		success = false;
+	}
 
-    while(!gotoAc_.waitForServer(ros::Duration(5.0)))
-      ROS_INFO("ChatCore:goto: goto received, but no move_base server. Waiting for the move_base action server to come up");
-
-    ROS_INFO("ChatCore::goto: sending goal to planner.");
-    gotoAc_.sendGoal(goal->goal_move_base);
-    gotoAc_.waitForResult();
-
-    if(success && gotoAc_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
+	if (success && gotoAc_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+	{
       ROS_INFO("ChatCore: goto completed!");
   	  chatbot::NamedMoveBaseResult res;
       gotoAs_.setSucceeded(res, "success");
@@ -68,7 +66,7 @@ void ChatCore::executePickCB(const chatbot::NamedMoveBaseGoalConstPtr &goal)
     ROS_INFO("ChatCore::pick: %s[%.2f,%.2f] received from prolog!", name.c_str(), x, y);
 
     // start executing the action
-
+    ros::Duration(20).sleep();
     // check that preempt has not been requested by the client
     if (pickAs_.isPreemptRequested() || !ros::ok())
     {
@@ -77,7 +75,6 @@ void ChatCore::executePickCB(const chatbot::NamedMoveBaseGoalConstPtr &goal)
         pickAs_.setPreempted();
         success = false;
     }
-    ros::Duration(20).sleep();
 
     if(success)
     {
@@ -103,7 +100,7 @@ void ChatCore::executePlaceCB(const chatbot::NamedMoveBaseGoalConstPtr &goal)
     ROS_INFO("ChatCore::place: %s[%.2f,%.2f] received from prolog!", name.c_str(), x, y);
 
     // start executing the action
-
+    ros::Duration(20).sleep();
     // check that preempt has not been requested by the client
     if (placeAs_.isPreemptRequested() || !ros::ok())
     {
@@ -112,7 +109,6 @@ void ChatCore::executePlaceCB(const chatbot::NamedMoveBaseGoalConstPtr &goal)
         placeAs_.setPreempted();
         success = false;
     }
-    ros::Duration(20).sleep();
 
     if(success)
     {
