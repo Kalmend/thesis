d26f1297d6a027a5bbd2f709bca97f53c9ce95f2
Oliver Kalmend <kalmendoliver@gmail.com>
2016-10-05 20:35:38 +0300

    minor param changes for voice recognition engine

--- voice_recognition_test.sh
+++ voice_recognition_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
-CONFDIR=/home/oliver/thesis/catkin_ws/deps/conf/ivector_extractor.fixed.conf
-export GST_PLUGIN_PATH=/home/oliver/thesis/catkin_ws/deps/
+CONFDIR=$BASEDIR/deps/conf/ivector_extractor.fixed.conf
+export GST_PLUGIN_PATH=$BASEDIR/deps
 echo "gst_plugin_path: $GST_PLUGIN_PATH"
 echo "config_path: $CONFDIR"
 
-gst-launch-1.0 --gst-debug="" -q alsasrc ! decodebin ! audioconvert ! audioresample ! kaldinnet2onlinedecoder \
+GST_DEBUG_FILE=test.txt gst-launch-1.0 --gst-debug-no-color --gst-debug="kaldinnet2onlinedecoder:5" -q alsasrc ! decodebin ! audioconvert ! audioresample ! kaldinnet2onlinedecoder \
   use-threaded-decoder=false \
   model=$GST_PLUGIN_PATH/final.mdl \
   fst=$GST_PLUGIN_PATH/HCLG.fst \
@@ -13,9 +13,9 @@ gst-launch-1.0 --gst-debug="" -q alsasrc ! decodebin ! audioconvert ! audioresam
   feature-type=mfcc \
   mfcc-config=$GST_PLUGIN_PATH/conf/mfcc.conf \
   ivector-extraction-config=$GST_PLUGIN_PATH/conf/ivector_extractor.fixed.conf \
-  max-active=10000 \
-  beam=10.0 \
-  lattice-beam=8.0 \
+  max-active=7000 \
+  beam=11.0 \
+  lattice-beam=5.0 \
   do-endpointing=true \
   endpoint-silence-phones="1:2:3:4:5:6:7:8:9:10" \
   chunk-length-in-secs=0.2 \
