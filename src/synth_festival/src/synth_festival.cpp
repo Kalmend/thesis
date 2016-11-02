1e4de682eaaefc972ccf0d73a26a46de5b4215c9
Oliver Kalmend <kalmendoliver@gmail.com>
2016-11-02 02:20:53 +0200

    synth_festival: using boost locale to handle umlauts in speech synthesis

--- src/synth_festival/src/synth_festival.cpp
+++ src/synth_festival/src/synth_festival.cpp
@@ -1,5 +1,6 @@
 #include "synth_festival.hpp"
-
+#include <boost/locale.hpp>
+using namespace boost::locale::conv;
 using namespace std;
 
 SynthesizerFestival::SynthesizerFestival()
@@ -19,27 +20,11 @@ int main(int argc, char **argv)
 	SynthesizerFestival client;
     	int heap_size = 2100000;
     	int load_init_files = 1; // we want the festival init files loaded
+
     	festival_initialize(load_init_files,heap_size);
-
     	festival_eval_command("(voice_eki_et_riina_clunits)");
-    	// Say some text;
+    	festival_say_text(from_utf(u8"jäärapäine ööbik õun","Latin9").c_str());
 
-	FILE *f = fopen("/tmp/test.txt", "rb");
-	fseek(f, 0, SEEK_END);
-	long fsize = ftell(f);
-	fseek(f, 0, SEEK_SET);
-	std::string arp = "jäärapäine ööbik õun";
-	const char* uarp = "jäärapäine ööbik õun";
-	
-	char *string = (char *)malloc(fsize + 1);
-	fread(string, fsize, 1, f);
-	fclose(f);
-
-	string[fsize] = 0;
-	std::cout << std::to_string((uint)string[0]) << "|" << std::to_string((uint)string[1]) << "|" << std::to_string((uint)string[2]) << string[3] << " x " << std::to_string((uint)uarp[0]) << "|" << std::to_string((uint)uarp[1]) << "|" << std::to_string((uint)uarp[2]) << "|" << std::to_string((uint)uarp[3]) << "|" << std::to_string((uint)uarp[4])  << " x "  << std::endl;
-    	festival_say_text(string);
-	//festival_say_text(arp.c_str());
-	festival_say_text(uarp);
 	ros::spin();
 	return 0;
 }
